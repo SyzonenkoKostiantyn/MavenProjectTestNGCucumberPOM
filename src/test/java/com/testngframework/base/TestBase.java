9ce86e3beba6073e880768524fce1b3021411a0e
Syzonenko90 <syzonenko.qa.test@gmail.com>
2023-07-01 14:45:08 -0400

    Framework Design

---
+++ src/test/java/com/testngframework/base/TestBase.java
@@ -0,0 +1,22 @@
+package com.testngframework.base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+    public static WebDriver driver;
+
+    @BeforeSuite
+    public void setUp(){
+
+
+    }
+
+    @AfterSuite
+    public void tearDown() {
+
+
+    }
+}
